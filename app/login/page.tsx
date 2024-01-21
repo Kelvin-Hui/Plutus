3c5258c2d99bcb68e93905b4403ea3277455db4b
Kelvin Hui <kc.kelvin.hui@gmail.com>
2024-01-21 16:44:56 -0500

    style(responsive): finished login page📄

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,12 +11,14 @@ import { SignupForm } from './components/signup-form';
 export default function LoginPage() {
   return (
     <div className="flex h-screen items-center justify-center bg-foreground/5">
-      <Card>
+      <Card className="m-4 w-full max-w-[500px]">
         <CardHeader>
           <CardTitle>
             <div className="flex flex-col items-center justify-center">
               <ArrowTrendingUpIcon className="h-24 w-24 animate-pulse text-green-600" />
-              <h1 className="font-sanif text-8xl font-bold">Plutus</h1>
+              <h1 className="text-5xl font-bold leading-tight tracking-tight">
+                Plutus
+              </h1>
               <span className="text-lg text-muted-foreground">
                 Welcome! Sign In To Continue
               </span>
@@ -25,7 +27,7 @@ export default function LoginPage() {
         </CardHeader>
         <CardContent>
           <Tabs defaultValue="Login">
-            <TabsList className="mb-8 grid w-[500px] grid-cols-2">
+            <TabsList className="mb-8 grid grid-cols-2">
               <TabsTrigger value="Login">Login</TabsTrigger>
               <TabsTrigger value="Signup">Sign Up</TabsTrigger>
             </TabsList>
@@ -40,5 +42,35 @@ export default function LoginPage() {
         </CardContent>
       </Card>
     </div>
+    // <div className="flex h-screen items-center justify-center bg-foreground/5">
+    //   <Card>
+    //     <CardHeader>
+    //       <CardTitle>
+    //         <div className="flex flex-col items-center justify-center">
+    //           <ArrowTrendingUpIcon className="h-24 w-24 animate-pulse text-green-600" />
+    //           <h1 className="text-8xl font-bold">Plutus</h1>
+    //           <span className="text-lg text-muted-foreground">
+    //             Welcome! Sign In To Continue
+    //           </span>
+    //         </div>
+    //       </CardTitle>
+    //     </CardHeader>
+    //     <CardContent>
+    //       <Tabs defaultValue="Login">
+    //         <TabsList className="mb-8 grid w-[500px] grid-cols-2">
+    //           <TabsTrigger value="Login">Login</TabsTrigger>
+    //           <TabsTrigger value="Signup">Sign Up</TabsTrigger>
+    //         </TabsList>
+
+    //         <TabsContent value="Login">
+    //           <LoginForm />
+    //         </TabsContent>
+    //         <TabsContent value="Signup">
+    //           <SignupForm />
+    //         </TabsContent>
+    //       </Tabs>
+    //     </CardContent>
+    //   </Card>
+    // </div>
   );
 }
